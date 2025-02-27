0e8b477348b0c0203e4f03225fa80f12e4791d7b
김영길/KIM YOUNG GIL <80146176+Gilpop8663@users.noreply.github.com>
2025-02-27 13:32:14 +0900

    chore: 'BASE_URL 변경'

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,8 +6,7 @@ export const LOCATION = 'Asia/Seoul';
 export const DATE_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 
 //SERVER
-export const BASE_URL =
-  'https://port-0-node-express-3vw25lci3y9wi.gksl2.cloudtype.app';
+export const BASE_URL = 'https://portfolio.coddink.com/api/fakeProjects';
 export const MESSEAGE_URL = `${BASE_URL}/messages`;
 export const LOGIN_URL = `${BASE_URL}/login`;
 export const PARAMS_USERS_URL = '/users';
